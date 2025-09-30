a7bb164e12fafcc63bfa60a6d9f3659bfbd2da71
Ben Yahmed Mohamed <benyahmed.mohamed17@gmail.com>
2025-09-30 17:51:46 -0400

    Delete netlify/functions/ping.js

--- netlify/functions/ping.js
+++
@@ -1,8 +0,0 @@
-// netlify/functions/ping.js
-exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    headers: { 'content-type': 'text/plain' },
-    body: `OK ${event.httpMethod} ${new Date().toISOString()}`,
-  };
-};
